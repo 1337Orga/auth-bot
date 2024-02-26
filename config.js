9af7dac1f122fa16afd0d3e56b9dab2852e09bfd
Nyxoy <137317152+Nyxoy201@users.noreply.github.com>
2024-02-26 21:31:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "token": "MTIxMDkzODYyMTU2NjM5MDI5Mg.GaRdxI.nGJVe8V1CKvIaeyCyh4LGP_QJDkNPIPpJz08KY", //token de ton bot
-  "client_secret": "kQwefAi6eztfNgI5YIcCaLGR7M1REoki", //client secret de ton bot
-  "mongodb": "mongodb+srv://40yd7:0b9hk9i@cluster0.efkbj9r.mongodb.net/?retryWrites=true&w=majority", //database mongo db
+  "token": "", //token de ton bot
+  "client_secret": "", //client secret de ton bot
+  "mongodb": "", //database mongo db
   "prefix": "+",
-  "owner": "1205243826717855845", //la personne qui pourra mettre owner des gens les autre pouront pas !
+  "owner": "", //la personne qui pourra mettre owner des gens les autre pouront pas !
   "cooldown100": true, //si il y aura du cooldown sur les commande joinall ou pas 
-  "support": "https://discord.gg/oauth2-bot", // serveur support 
-  "client_id": "1210938621566390292", //client id de ton bot
-  "redirect_uri": "http://154.51.39.87:20007/verify", //redirect que tu vas mettre dans les developer portal avec /verify a la fin exemple : https://authorize.oauth2-bot.site/verify
-  "oauth_link": "https://discord.com/oauth2/authorize?client_id=1210938621566390292&response_type=code&redirect_uri=http%3A%2F%2F154.51.39.87%3A20007%2Fverify&scope=identify+guilds.join", //auth links que tu a dans le dev portal
+  "support": "", // serveur support 
+  "client_id": "", //client id de ton bot
+  "redirect_uri": ", //redirect que tu vas mettre dans les developer portal avec /verify a la fin exemple : https://authorize.oauth2-bot.site/verify
+  "oauth_link": "", //auth links que tu a dans le dev portal
   "owners": [
     "", //touche pas ici c'est quand tu vas mettre des gens en wl
   ],
